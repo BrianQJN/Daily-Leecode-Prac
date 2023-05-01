dcbae2c7004b0ebe5b99f8a436642a8cb374f1dd
BrianQu <qujianning0401@163.com>
2023-05-01 19:41:17 -0400

    2023.05.01 leetcode_5

--- 5_Longest_Palindromic_Substring.py
+++ 5_Longest_Palindromic_Substring.py
@@ -41,40 +41,6 @@ class Solution(object):
         
         return longest_substr
     
-    def longestPalindromicSubstring_2(self, s):
-        cur_substr = ""
-        max_len = 0
-        longest_substr = ""
-        pointer = 0
-        sub_pointer = 0
-
-        if len(s) == 1:
-            return s
-        else:
-            for char in s:
-                cur_substr += char
-                if char in s[pointer+1:]:
-                    for sub_char in s[pointer+1:]:
-                        cur_substr += sub_char
-                        if cur_substr == cur_substr[::-1]:
-                            if len(cur_substr) > max_len:
-                                longest_substr = cur_substr
-                                max_len = len(cur_substr)
-                        if char in s[pointer+1:][sub_pointer+1:]:
-                            sub_pointer += 1
-                            continue
-                        else:
-                            break
-                else:
-                    if len(cur_substr) > max_len:
-                        longest_substr = cur_substr
-                        max_len = len(cur_substr)
-
-                cur_substr = ""
-                pointer += 1
-        
-        return longest_substr
-    
 
 if __name__ == "__main__":
     test = Solution
