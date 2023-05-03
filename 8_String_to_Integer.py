a15f68bcbfd55dde95e3dab8cb2631e75515540f
BrianQu <qujianning0401@163.com>
2023-05-03 16:23:37 -0400

    2023.05.03 leetcode_8

---
+++ 8_String_to_Integer.py
@@ -0,0 +1,102 @@
+"""
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
+
+The algorithm for myAtoi(string s) is as follows:
+
+1. Read in and ignore any leading whitespace.
+2. Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+3. Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+4. Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+5. If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
+6. Return the integer as the final result.
+
+Example 1:
+Input: s = "42"
+Output: 42
+Explanation: The underlined characters are what is read in, the caret is the current reader position.
+Step 1: "42" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "42" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "42" ("42" is read in)
+           ^
+The parsed integer is 42.
+Since 42 is in the range [-231, 231 - 1], the final result is 42.
+
+Example 2:
+Input: s = "   -42"
+Output: -42
+Explanation:
+Step 1: "   -42" (leading whitespace is read and ignored)
+            ^
+Step 2: "   -42" ('-' is read, so the result should be negative)
+             ^
+Step 3: "   -42" ("42" is read in)
+               ^
+The parsed integer is -42.
+Since -42 is in the range [-231, 231 - 1], the final result is -42.
+
+Example 3:
+Input: s = "4193 with words"
+Output: 4193
+Explanation:
+Step 1: "4193 with words" (no characters read because there is no leading whitespace)
+         ^
+Step 2: "4193 with words" (no characters read because there is neither a '-' nor '+')
+         ^
+Step 3: "4193 with words" ("4193" is read in; reading stops because the next character is a non-digit)
+             ^
+The parsed integer is 4193.
+Since 4193 is in the range [-231, 231 - 1], the final result is 4193.
+"""
+class Solution(object):
+    def stringToInteger(self, s):
+        """
+        1. Read in and ignore any leading whitespace.
+        2. Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
+        3. Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
+        4. Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
+        5. If the integer is out of the 32-bit signed integer range [-2^31, 2^31 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -2^31 should be clamped to -2^31, and integers greater than 2^31 - 1 should be clamped to 2^31 - 1.
+        6. Return the integer as the final result.
+        """
+        output = ""
+        isNegative = False
+        s = s.lstrip()
+        if s == "":
+            return 0
+
+        if s[0] == '-':
+            isNegative = True
+            s = s[1:]
+        elif s[0] == '+':
+            s = s[1:]
+
+        for char in s:
+            if char.isdigit():
+                output += char
+            else:
+                break
+        
+        if output == "":
+            return 0
+        
+        output = int(output)
+
+        if isNegative:
+            output = -output
+        
+        if output < -2**31:
+            return -2**31
+        elif output > 2**31-1:
+            return 2**31-1
+        
+        return output
+    
+
+if __name__ == "__main__":
+    test = Solution
+    print(test.stringToInteger(test, "   123"))
+    print(test.stringToInteger(test, "   -42"))
+    print(test.stringToInteger(test, "4193 with words"))
+    print(test.stringToInteger(test, ""))
+    print(test.stringToInteger(test, " "))
