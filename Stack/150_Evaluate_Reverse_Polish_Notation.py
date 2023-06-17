4a264d3d7e8e1374df6906dfba1d2860e3f1f185
BrianQu <qujianning0401@163.com>
2023-06-17 17:18:49 -0400

    update

---
+++ Stack/150_Evaluate_Reverse_Polish_Notation.py
@@ -0,0 +1,59 @@
+"""
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+
+The valid operators are '+', '-', '*', and '/'.
+Each operand may be an integer or another expression.
+The division between two integers always truncates toward zero.
+There will not be any division by zero.
+The input represents a valid arithmetic expression in a reverse polish notation.
+The answer and all the intermediate calculations can be represented in a 32-bit integer.
+
+Example 1:
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+
+Example 2:
+Input: tokens = ["4","13","5","/","+"]
+Output: 6
+Explanation: (4 + (13 / 5)) = 6
+
+Example 3:
+Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+Output: 22
+Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+= ((10 * (6 / (12 * -11))) + 17) + 5
+= ((10 * (6 / -132)) + 17) + 5
+= ((10 * 0) + 17) + 5
+= (0 + 17) + 5
+= 17 + 5
+= 22
+"""
+class Solution(object):
+    def evaluateRPN(self, tokens):
+        stack = []
+
+        for token in tokens:
+            if token == '+':
+                stack.append(stack.pop() + stack.pop())
+            elif token == '-':
+                a, b  = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif token == '*':
+                stack.append(stack.pop() * stack.pop())
+            elif token == '/':
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            else:
+                stack.append(int(token))
+        
+        return stack[0]
+    
+
+if __name__ == "__main__":
+    test = Solution
+    print(test.evaluateRPN(test, ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
