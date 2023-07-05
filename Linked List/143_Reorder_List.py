c50b405be1e2920fd44216ccff844f831bf0daae
BrianQu <qujianning0401@163.com>
2023-07-05 03:22:20 -0400

    update

--- Linked List/143_Reorder_List.py
+++ Linked List/143_Reorder_List.py
@@ -1,20 +1,27 @@
 """
+You are given the head of a singly linked-list. The list can be represented as:
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
 
+Example:
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
 """
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 class Solution:
     def reorderList(self, head: ListNode) -> None:
-        # find middle
+        # find the middle of the linklist
         slow, fast = head, head.next
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next.next
-
-        # reverse second half
+            fast = slow.next.next
+        
+        # reverse the second half
         second = slow.next
         prev = slow.next = None
         while second:
@@ -22,11 +29,12 @@ class Solution:
             second.next = prev
             prev = second
             second = tmp
-
-        # merge two halfs
+        
+        # merge the two halfs:
         first, second = head, prev
         while second:
             tmp1, tmp2 = first.next, second.next
             first.next = second
             second.next = tmp1
             first, second = tmp1, tmp2
+        
