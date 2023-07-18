f815963fb86b918a8cdd889763939a3e10f6679a
BrianQu <qujianning0401@163.com>
2023-07-18 19:03:05 -0400

    update

---
+++ Trie/211_Design_Add_and_Search_Words_Data_Structure.py
@@ -0,0 +1,17 @@
+class WordDictionary(object):
+
+    def __init__(self):
+
+
+    def addWord(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
