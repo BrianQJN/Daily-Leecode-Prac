ce78c65a26196e42e38c1f87886c1a438343f7fb
BrianQu <qujianning0401@163.com>
2023-07-18 19:10:50 -0400

    update

--- Trie/211_Design_Add_and_Search_Words_Data_Structure.py
+++ Trie/211_Design_Add_and_Search_Words_Data_Structure.py
@@ -6,6 +6,7 @@ class TrieNode:
 class WordDictionary(object):
 
     def __init__(self):
+        self.root = TrieNode()
 
 
     def addWord(self, word):
@@ -13,6 +14,14 @@ class WordDictionary(object):
         :type word: str
         :rtype: None
         """
+        cur = self.root
+
+        for char in word:
+            if char not in cur.children:
+                cur.children[char] = TrieNode()
+            cur = cur.children[char]
+        
+        cur.end = True
 
 
     def search(self, word):
