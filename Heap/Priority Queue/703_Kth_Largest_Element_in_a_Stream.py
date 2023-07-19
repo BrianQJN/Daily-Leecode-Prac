83534f1884278a6768cb39d8ae610e5a3a00de2e
BrianQu <qujianning0401@163.com>
2023-07-19 03:41:44 -0400

    update

--- Heap/Priority Queue/703_Kth_Largest_Element_in_a_Stream.py
+++ Heap/Priority Queue/703_Kth_Largest_Element_in_a_Stream.py
@@ -20,3 +20,27 @@ kthLargest.add(10);  // return 5
 kthLargest.add(9);   // return 8
 kthLargest.add(4);   // return 8
 """
+import heapq
+
+
+class KthLargest(object):
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.minHeap, self.k = nums, k
+        heapq.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            heapq.heappop(self.minHeap)
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        heapq.heappush(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+        return self.minHeap[0]
