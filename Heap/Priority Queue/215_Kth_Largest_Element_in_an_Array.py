8722e72ca511eac5b3b6a13b02596c50c7a9f3ae
BrianQu <qujianning0401@163.com>
2023-07-22 07:20:16 -0400

    update

--- Heap/Priority Queue/215_Kth_Largest_Element_in_an_Array.py
+++ Heap/Priority Queue/215_Kth_Largest_Element_in_an_Array.py
@@ -27,3 +27,32 @@ class Solution(object):
             smallest = heapq.heappop(nums)
             if i == k - 1:
                 return -smallest
+            
+    def partition(self, nums, left, right):
+        """
+        Using this function, you can get the array that the elements at the left of the nums[left] are less than nums[left] while
+        the elements at the right of the nums[left] are greater than nums[left]
+        """
+        # select the most left element as the pivot
+        pivot = nums[left]
+
+        # using double pointers
+        l, r = left, right
+
+        while l < r:
+            # find the first element less than the pivot from right
+            while l < r and nums[r] >= pivot:
+                r -= 1
+            nums[l] = nums[r]
+
+            # find the first element greater than the pivot from left
+            while l < r and nums[l] <= pivot:
+                l += 1
+            nums[r] = nums[l]
+
+        # put the pivot to the position where the left pointer is equal to the right pointer
+        nums[l] = pivot
+
+        # return l is the same as return r, because l == r
+        return l
+    
