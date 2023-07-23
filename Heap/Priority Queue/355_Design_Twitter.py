af8782e5a690d4c8e38239fef687d6f1cbd3cff1
BrianQu <qujianning0401@163.com>
2023-07-23 15:04:09 -0400

    update

---
+++ Heap/Priority Queue/355_Design_Twitter.py
@@ -0,0 +1,80 @@
+"""
+Design a simplified version of Twitter where users can post tweets, follow/unfollow another user, and is able to see the 10 most recent tweets in the user's news feed.
+
+Implement the Twitter class:
+
+Twitter() Initializes your twitter object.
+void postTweet(int userId, int tweetId) Composes a new tweet with ID tweetId by the user userId. Each call to this function will be made with a unique tweetId.
+List<Integer> getNewsFeed(int userId) Retrieves the 10 most recent tweet IDs in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user themself. Tweets must be ordered from most recent to least recent.
+void follow(int followerId, int followeeId) The user with ID followerId started following the user with ID followeeId.
+void unfollow(int followerId, int followeeId) The user with ID followerId started unfollowing the user with ID followeeId.
+"""
+from collections import defaultdict
+import heapq
+
+
+class Twitter(object):
+
+    def __init__(self):
+        # using count to record the time the tweet was created
+        self.count = 0
+        # userId -> list of [count, tweedId]
+        self.tweetMap = defaultdict(list)
+        # userId -> set of followeeId
+        self.followMap = defaultdict(set)
+
+    def postTweet(self, userId, tweetId):
+        """
+        :type userId: int
+        :type tweetId: int
+        :rtype: None
+        """
+        self.tweetMap[userId].append([self.count, tweetId])
+        self.count -= 1
+
+
+    def getNewsFeed(self, userId):
+        """
+        :type userId: int
+        :rtype: List[int]
+        """
+        res = []
+        minHeap = []
+
+        self.followMap[userId].add(userId)
+
+        # since that the tweets for every user are added in time order, so this part is to extract the most recently tweet for every user
+        # so we can compare these most recently tweets, and then add next tweet in that user, then to compare again
+        for followeeId in self.followMap[userId]:
+            if followeeId in self.tweetMap:
+                index = len(self.tweetMap[followeeId]) - 1
+                count, tweetId = self.tweetMap[followeeId][index]
+                heapq.heappush(minHeap, [count, tweetId, followeeId, index - 1])
+
+        while minHeap and len(res) < 10:
+            count, tweetId, followeeId, index = heapq.heappop(minHeap)
+            res.append(tweetId)
+            if index >= 0:
+                count, tweetId = self.tweetMap[followeeId][index]
+                heapq.heappush(minHeap, [count, tweetId, followeeId, index - 1])
+
+        return res
+
+
+    def follow(self, followerId, followeeId):
+        """
+        :type followerId: int
+        :type followeeId: int
+        :rtype: None
+        """
+        self.followMap[followerId].add(followeeId)
+
+
+    def unfollow(self, followerId, followeeId):
+        """
+        :type followerId: int
+        :type followeeId: int
+        :rtype: None
+        """
+        if followeeId in self.followMap[followerId]:
+            self.followMap[followerId].remove(followeeId)
