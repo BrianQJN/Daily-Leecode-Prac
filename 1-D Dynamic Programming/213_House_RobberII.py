254324bf1c78b681c1dcbe3460b331b1ca7f54e9
BrianQu <qujianning0401@163.com>
2023-07-31 10:53:29 -0400

    update

--- 1-D Dynamic Programming/213_House_RobberII.py
+++ 1-D Dynamic Programming/213_House_RobberII.py
@@ -21,16 +21,24 @@ class Solution(object):
 
         if n == 2:
             return max(nums[0], nums[1])
-
+        
+        # Since there is a cycle, so we need to rob from 0 to n-2 or rob from 1 to n-1
+        # we can create this help function to do the dynamic operation in different ranges
         def rob_range(start, end):
+            # get cur array nums of the amount of money of each house
             cur_nums = nums[start:end]
             m = len(cur_nums)
+
             dp = [0] * m
             dp[0] = cur_nums[0]
             dp[1] = max(cur_nums[0], cur_nums[1])
+
+            # since we can not rob the houses were adjacent
+            # thus, for each house, if we rob it, we can get cur_nums[i] + dp[i-2], else we can get dp[i-1].
             for i in range(2, m):
                 dp[i] = max(cur_nums[i] + dp[i-2], dp[i-1])
             return dp[m-1]
         
+        # return the maximum of these two ranges of robbing plans
         return max(rob_range(0, n-1), rob_range(1, n))
         
