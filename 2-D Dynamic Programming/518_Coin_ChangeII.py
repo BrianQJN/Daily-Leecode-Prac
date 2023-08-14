eaf0907e5a3f4c37669303b3873fde9b24738f32
BrianQu <qujianning0401@163.com>
2023-08-13 23:30:07 -0400

    update

--- 2-D Dynamic Programming/518_Coin_ChangeII.py
+++ 2-D Dynamic Programming/518_Coin_ChangeII.py
@@ -23,7 +23,7 @@ class Solution:
         dp[0] = 1
 
         for coin in coins:
-            for i in range(1, amount+1):
+            for i in range(coin, amount+1):
                 dp[i] += dp[i - coin]
 
         return dp[amount]
