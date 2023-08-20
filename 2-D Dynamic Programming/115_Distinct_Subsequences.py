3db9ba285295471a60dbe6e0c217913c4f44db60
BrianQu <qujianning0401@163.com>
2023-08-19 23:05:56 -0400

    update

---
+++ 2-D Dynamic Programming/115_Distinct_Subsequences.py
@@ -0,0 +1,14 @@
+"""
+Given two strings s and t, return the number of distinct subsequences of s which equals t.
+
+The test cases are generated so that the answer fits on a 32-bit signed integer.
+
+Example:
+Input: s = "rabbbit", t = "rabbit"
+Output: 3
+Explanation:
+As shown below, there are 3 ways you can generate "rabbit" from s.
+rab b bit
+rabb b it
+rab b bit
+"""
